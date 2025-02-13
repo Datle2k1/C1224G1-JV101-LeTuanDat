44cba3906e3044c9d335c6bc844e795153505043
Datle2k1 <datletuan110621@gmail.com>
2025-02-13 08:45:25 +0700

    Bài 4 - Thực hành - Tạo class hình chữ nhật

---
+++ module 2/week 1/create-rectangle-class/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width:");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height:");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+}
