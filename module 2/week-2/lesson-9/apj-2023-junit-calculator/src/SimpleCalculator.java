8f64694aa3b7d4d2a7ec07bf6a9e2899ed81fe77
Datle2k1 <datletuan110621@gmail.com>
2025-02-20 12:04:44 +0700

    delete

--- module 2/week-2/lesson-9/apj-2023-junit-calculator/src/SimpleCalculator.java
+++ module 2/week-2/lesson-9/apj-2023-junit-calculator/src/SimpleCalculator.java
@@ -1,11 +1,9 @@
-package src;
-
 public class SimpleCalculator {
     public static int add(int first, int second){
-        return first + second;
+        throw new UnsupportedOperationException();
     }
 
     public static int sub(int first, int second){
-        return first - second;
+        throw new UnsupportedOperationException();
     }
 }
--- module 2/week-2/lesson-9/apj-2023-junit-calculator/src/SimpleCalculatorTest.java
+++ module 2/week-2/lesson-9/apj-2023-junit-calculator/src/SimpleCalculatorTest.java
@@ -1,11 +1,3 @@
-package src;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 public class SimpleCalculatorTest {
     @Test
     @DisplayName("Testing add 0 + 0")
