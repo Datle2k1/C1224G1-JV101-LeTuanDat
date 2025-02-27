85c9978612c0fe0cabf6f369b4088b7e5c083b34
Datle2k1 <datletuan110621@gmail.com>
2025-02-27 10:13:43 +0700

    Bài 14 - Thực hành - Triển khai thuật toán selection sort

---
+++ module 2/week-3/lesson-14/practise-selectionsort/src/SelectionSortTest.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SelectionSortTest {
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 2, 7, 4, 5, 3, 8, 6, 9, 1, 3};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
