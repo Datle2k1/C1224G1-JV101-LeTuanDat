ce51d07f5d4cf56c78addaf2ad7b5ac1fbd2e787
Datle2k1 <datletuan110621@gmail.com>
2025-03-04 09:25:13 +0700

    Bài 17 - Thực hành - Đọc và ghi danh sách sinh viên ra file nhị phân

---
+++ module 2/week-4/lesson-17/practise-read_write-to-binary-fiel/src/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void writeDataToFile(String path, List<Student> students) {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream oos = new ObjectOutputStream(fos);){
+            oos.writeObject(students);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Student> readDataFromFile(String path){
+        List<Student> students = new ArrayList<>();
+        try{
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            students = (List<Student>) ois.readObject();
+            fis.close();
+            ois.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return students;
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Vũ Kiều Anh", "Hà Nội"));
+        students.add(new Student(2, "Nguyễn Minh Quân", "Hà Nội"));
+        students.add(new Student(3, "Đặng Huy Hoà", "Đà Nẵng"));
+        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
+        students.add(new Student(5, "Nguyễn Khắc Nhật", "Hà Nội"));
+
+        writeDataToFile("src/student.txt", students);
+
+        for (Student student: readDataFromFile("src/student.txt")) {
+            System.out.println(student);
+        }
+    }
+}
