18c8d1a163c94a10ac1f6475c0eb2754cf0c7e6a
Datle2k1 <datletuan110621@gmail.com>
2025-03-04 09:11:29 +0700

    Bài 17 - Thực hành - Copy file có dung lượng lớn

---
+++ module 2/week-4/lesson-17/practise-copy-large-file/src/Main.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = Files.newInputStream(source.toPath());
+            os = Files.newOutputStream(dest.toPath());
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            if (is != null && os != null) {
+                is.close();
+                os.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter source file:");
+        String sourcePath = in.nextLine();
+        System.out.print("Enter destination file:");
+        String destPath = in.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingStream(sourceFile, destFile);
+            System.out.print("Copy completed");
+        } catch (IOException ioe) {
+            System.out.print("Can't copy that file");
+            System.out.printf(ioe.getMessage());
+        }
+    }
+}
